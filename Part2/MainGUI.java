18be1999e15a750adb6a000407a1ece5a976262e
Abdullahi Mohammed <ec25632@qmul.ac.uk>
2026-04-28 22:38:17 +0100

    Added title label and improved GUI layout

--- Part2/MainGUI.java
+++ Part2/MainGUI.java
@@ -1,16 +1,24 @@
 package Part2;
+import java.awt.BorderLayout;
+
 import javax.swing.*;
+import java.awt.Font;
 
 public class MainGUI {
     public static void main(String[] args) {
         // Create the main frame
         JFrame frame = new JFrame ("Typing Race GUI");
-        frame.setSize(600, 400);
+        frame.setSize(700, 450);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new BorderLayout());
+
+        JLabel titleLabel = new JLabel("Typing Race Simulator" , SwingConstants.CENTER);
+        titleLabel.setFont(new Font("Arial", Font.BOLD, 28));
 
         JButton startButton = new JButton("Start Race");
 
-        frame.add(startButton);
+        frame.add(titleLabel, BorderLayout.NORTH);
+        frame.add(startButton, BorderLayout.CENTER);
         frame.setVisible(true);
     }
 }
