4d6f5d44cc806e0e3ad5413770a4890b1b27ccd1
Abdullahi Mohammed <ec25632@qmul.ac.uk>
2026-04-29 17:38:25 +0100

    Added progress bars to the GUI

--- Part2/MainGUI.java
+++ Part2/MainGUI.java
@@ -3,6 +3,7 @@ import java.awt.BorderLayout;
 
 import javax.swing.*;
 import java.awt.Font;
+import java.awt.GridLayout;
 import java.awt.event.*;
 
 public class MainGUI {
@@ -20,14 +21,38 @@ public class MainGUI {
 
         JLabel statusLabel = new JLabel("Click 'Start Race' to begin.", SwingConstants.CENTER);
 
+        JPanel racePanel = new JPanel();
+        racePanel.setLayout(new GridLayout(3,1));
+
+        JProgressBar aliceBar = new JProgressBar(0, 100);
+        aliceBar.setStringPainted(true);
+        aliceBar.setString("ALICE");
+
+        JProgressBar bobBar = new JProgressBar(0, 100);
+        bobBar.setStringPainted(true);
+        bobBar.setString("BOB");
+
+        JProgressBar charlieBar = new JProgressBar(0, 100);
+        charlieBar.setStringPainted(true);
+        charlieBar.setString("CHARLIE");
+
+        racePanel.add(aliceBar);
+        racePanel.add(bobBar);
+        racePanel.add(charlieBar);
+
         startButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 statusLabel.setText("Race Started!");
+            
+            aliceBar.setValue(85);
+            bobBar.setValue(60);
+            charlieBar.setValue(40);
             }
         });
 
         frame.add(titleLabel, BorderLayout.NORTH);
-        frame.add(startButton, BorderLayout.CENTER);
+        frame.add(racePanel, BorderLayout.CENTER);
+        frame.add(startButton, BorderLayout.WEST);
         frame.add(statusLabel, BorderLayout.SOUTH);
         frame.setVisible(true);
     }
